5a92d31a957343e0be6a99f751c2f423492069d2
unlimitedTing <39999422+unlimitedTing@users.noreply.github.com>
2023-04-30 19:41:07 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Overview from './component/Overview';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>React Coding Exercise</h1>
+      </header>
+      <section className="App-section">
+        <Overview />
+      </section>
+    </div>
+  );
+}
+
+export default App;
